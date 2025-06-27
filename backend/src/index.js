e740c3f04df762520157bf67a7baabbf94e10801
suraj-shrivastav <suraj.shrivastavvv@gmail.com>
2025-06-27 13:25:11 +0530

    .

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,10 +30,10 @@ app.use("/api/auth", authroutes);
 app.use("/api/messages", messageRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.json(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.json(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
